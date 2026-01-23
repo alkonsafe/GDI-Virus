ad3e291d51a2ac79f42477b79139577300bfdbd1
alkonsafe <philblakeschool@outlook.com>
2026-01-23 08:11:04 -0500

    Create getpath.hpp

---
+++ GDI+ test/GDI+ test/getpath.hpp
@@ -0,0 +1,20 @@
+// remade from the birdvirus
+#include <filesystem>
+#include <string>
+
+#include <windows.h>
+#include <shlobj.h>
+#include <objbase.h>
+
+#pragma comment(lib,"Shell32")
+#pragma comment(lib,"Ole32")
+
+std::filesystem::path
+get_desktop_path()
+{
+	wchar_t* p;
+	if (S_OK != SHGetKnownFolderPath(FOLDERID_Desktop, 0, NULL, &p)) return "";
+	std::filesystem::path result = p;
+	CoTaskMemFree(p);
+	return result;
+}
